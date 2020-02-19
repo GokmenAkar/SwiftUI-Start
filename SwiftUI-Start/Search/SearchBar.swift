800b077f76815a6f221536da5e74b5abc968c121
GokmenAkar <gokmen.akar92@gmail.com>
2020-02-19 16:54:29 +0300

    add search bar

---
+++ SwiftUI-Start/Search/SearchBar.swift
@@ -0,0 +1,40 @@
+//
+//  SearchBar.swift
+//  SwiftUI-Start
+//
+//  Created by Developer on 19.02.2020.
+//  Copyright © 2020 Developer. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+
+struct SearchBar: UIViewRepresentable {
+    @Binding var text: String
+    
+    class Coordinator: NSObject, UISearchBarDelegate {
+        @Binding var text: String
+        init(text: Binding<String>) {
+            _text = text
+        }
+        
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            text = searchText
+        }
+    }
+    
+    func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchBar>) {
+        uiView.text = text
+    }
+    
+    func makeCoordinator() -> SearchBar.Coordinator {
+        return Coordinator(text: $text)
+    }
+    
+    func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
+        let searchBar = UISearchBar(frame: .zero)
+        searchBar.delegate = context.coordinator
+        return searchBar
+    }
+    
+}
---
+++ SwiftUI-Start/Search/SearchView.swift
@@ -0,0 +1,32 @@
+//
+//  SearchView.swift
+//  SwiftUI-Start
+//
+//  Created by Developer on 19.02.2020.
+//  Copyright © 2020 Developer. All rights reserved.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    let names = ["Azam", "Gokmen", "Alex", "Shio", "John", "Kemal", "Istanbul"]
+    
+    @State private var searchTerm: String = ""
+    var body: some View {
+        List {
+            SearchBar(text: $searchTerm)
+            
+            ForEach(names.filter {
+                self.searchTerm.isEmpty ? true : $0.localizedCaseInsensitiveContains(self.searchTerm)
+            }, id: \.self) { name in
+                Text(name)
+            }
+        }
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
