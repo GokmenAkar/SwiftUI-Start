b0801a8c04814ee9642b732df6ac29f93704e664
GokmenAkar <gokmen.akar92@gmail.com>
2020-03-09 14:57:06 +0300

    - trim shapes by percentage

---
+++ SwiftUI-Start/Shapes/StrokeView.swift
@@ -0,0 +1,64 @@
+//
+//  StrokeView.swift
+//  SwiftUI-Start
+//
+//  Created by Developer on 9.03.2020.
+//  Copyright © 2020 Developer. All rights reserved.
+//
+
+import SwiftUI
+
+struct StrokeView: View {
+    @State private var circleProgress: CGFloat = 0.8
+    private var circlePercentage: Int { Int(circleProgress * 100.0) }
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                ZStack {
+                    Rectangle().padding(20).foregroundColor(Color.yellow)
+                    Rectangle().padding(40).foregroundColor(Color.red)
+                    Rectangle().padding(60).foregroundColor(Color.blue)
+                    Rectangle().padding(80).foregroundColor(Color.black)
+                }.frame(width: 200, height: 200)
+                
+                ZStack(alignment: .bottomLeading) {
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color.yellow)
+                        .frame(width: 320, height: 250)
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color.red)
+                        .frame(width: 280, height: 200)
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color.blue)
+                        .frame(width: 230, height: 160)
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color.black)
+                        .frame(width: 200, height: 120)
+                }
+                
+                Rectangle()
+                    .trim(from: 0, to: circleProgress)
+                    .stroke(Color.blue, style: StrokeStyle(lineWidth: 12, lineCap: .square))
+                    .frame(height: 300)
+                    .overlay(Text("\(circlePercentage)%").font(.largeTitle))
+                    .padding(40)
+                
+                VStack {
+                    Text("Progress")
+                    HStack {
+                        Text("0%")
+                        Slider(value: $circleProgress)
+                        Text("100%")
+                    }
+                }.padding(40)
+            }
+        }
+    }
+}
+
+struct StrokeView_Previews: PreviewProvider {
+    static var previews: some View {
+        StrokeView()
+    }
+}
